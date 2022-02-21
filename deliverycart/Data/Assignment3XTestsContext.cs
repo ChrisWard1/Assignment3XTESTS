27ec2357ce1127000c72c7e0e2107aa6e4672a7a
Chris Ward <cjward1@buffs.wtamu.edu>
2022-02-20 18:51:27 -0600

    Initial commit

--- deliverycart/Data/Assignment3XTestsContext.cs
+++ deliverycart/Data/Assignment3XTestsContext.cs
@@ -16,11 +16,11 @@ namespace deliverycart.Data
         }
 
         public DbSet<deliverycart.Models.Order> Order { get; set; }
-        // public DbSet<deliverycart.Models.Item> Item { get; set; }
-        // public DbSet<deliverycart.Models.Customer> Customer { get; set; }
-        // public DbSet<deliverycart.Models.Vendor> Vendor { get; set; }
-        // public DbSet<deliverycart.Models.Manager> Manager { get; set; }
-        // public DbSet<deliverycart.Models.Shopper> Shopper { get; set; }
+        public DbSet<deliverycart.Models.Item> Item { get; set; }
+        public DbSet<deliverycart.Models.Customer> Customer { get; set; }
+        public DbSet<deliverycart.Models.Vendor> Vendor { get; set; }
+        public DbSet<deliverycart.Models.Manager> Manager { get; set; }
+        public DbSet<deliverycart.Models.Shopper> Shopper { get; set; }
 
         #region snippet2
         public async virtual Task AddOrderAsync (deliverycart.Models.Order order){
